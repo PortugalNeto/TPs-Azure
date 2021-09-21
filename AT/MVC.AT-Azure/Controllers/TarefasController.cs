b0782bcc4b5336df02df4955d3739d0dd3ae0542
Paulo Portugal <portugneto@gmail.com>
2021-09-21 20:00:45 -0300

    ok

--- AT/MVC.AT-Azure/Controllers/TarefasController.cs
+++ AT/MVC.AT-Azure/Controllers/TarefasController.cs
@@ -3,18 +3,32 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using RestSharp;
+using MVC.AT_Azure.Models;
+using RestSharp.Serialization.Json;
 
 namespace MVC.AT_Azure.Controllers
 {
     public class TarefasController : Controller
     {
 
+        public readonly string url = "https://atazureinfnet.azurewebsites.net/api/";
+
         [HttpGet]
         public IActionResult Index()
         {
+            var client = new RestClient(url);
+            
+            var request = new RestRequest("GetAll", Method.GET);
+            
+            IRestResponse<List<Tarefa>> tar = client.Execute<List<Tarefa>>(request);
+            
+            var tarefas = tar.Data;
+            ViewBag.ListTarefas = tarefas;
+            
             return View();
         }
-        
+
         [HttpGet]
         public IActionResult Cadastrar()
         {
---
+++ AT/MVC.AT-Azure/Models/Tarefa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC.AT_Azure.Models
+{
+    public class Tarefa
+    {
+        public Guid Id { get; set; }
+        public string ToDo { get; set; } = "Tarefa";
+        public string NomeTarefa { get; set; }
+        public string Descricao { get; set; }
+        public string Status { get; set; }
+        public string Quem { get; set; }
+    }
+}
